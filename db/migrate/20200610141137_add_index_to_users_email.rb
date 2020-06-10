303abb5697b848367963a289eb8480a0af573b52
Pavel Barkov <thakur.ids@gmail.com>
2020-06-10 22:18:10 +0300

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20200610141137_add_index_to_users_email.rb
@@ -0,0 +1,7 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
+  def change
+  	# метот рельс add_index добавляет индекс в столбец электронной почты.
+  	# опция true создаёт уникальность
+  	add_index :users, :email, unique: true
+  end
+end
