5f49746e873f73cc11cfd205b82600c3ca83c2ee
Pavel Barkov <thakur.ids@gmail.com>
2020-08-10 23:50:16 +0300

    Восстановление пароля

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,17 +2,16 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation" do |format|
-    format.text(content_transfer_encoding: "7bit")
-    format.html(content_transfer_encoding: "7bit")
+    mail to: user.email, subject: "Активация аккаунта" do |format|
+      format.text(content_transfer_encoding: "7bit")
+      format.html(content_transfer_encoding: "7bit")
     end
 
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Восстановление пароля"
   end
 
 end
